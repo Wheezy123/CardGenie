50a3813ad2bf8aeb80ec63301547793b50c4e645
Jerome Johnson <JohnsonJeromeC@gmail.com>
2013-12-07 22:25:03 -0600

    added first and last name to users, added basic authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131129232835) do
+ActiveRecord::Schema.define(version: 20131207022909) do
 
   create_table "contacts", force: true do |t|
     t.string   "last_name"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20131129232835) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
